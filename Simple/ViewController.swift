81539b72e6b0eba352947a44e288c061910eccc5
truongminhluong <truongminhluong2003@gmail.com>
2023-11-01 19:41:14 +0700

    Initial Commit

---
+++ Simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple
+//
+//  Created by Trương Minh Lượng Jr on 01/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
